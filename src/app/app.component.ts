2bd004c27f17132c913f1923d5b088ab432a21f7
Avram Virgil <av.virgil@gmail.com>
2016-11-02 00:49:13 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MarkerService} from "./services/marker.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,96 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  //zoom level
+  zoom: number = 10;
+   //start position
+  lat: number = 42.858217;
+  lng: number = -70.929990;
+
+  //Values
+  markerName: string;
+  markerLat: string;
+  markerLng: string;
+  markerDraggable: string;
+
+  // Markers
+  markers: marker[] = [];
+
+  constructor(private markerService: MarkerService){
+
+    this.markers = this.markerService.getMarkers()
+  }
+
+  clickedMarker(marker: marker, index: number){
+    console.log('Clicked Marker' + marker.name + 'at index' + index)
+  }
+
+  mapClicked($event: any){
+    var newMarker = {
+      name: 'Untitled',
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: false
+    }
+
+    this.markers.push(newMarker);
+
+  }
+
+  markerDragEnd(marker: any, $event:any){
+    console.log('dragEnd',marker,$event)
+    var updMarker = {
+      name: marker.name,
+      lat: parseFloat(marker.lat),
+      lng: parseFloat(marker.lng),
+      draggable: false
+    }
+
+    var newLat = $event.coords.lat;
+    var newLng = $event.coords.lng;
+
+    this.markerService.updateMarker(updMarker, newLat, newLng)
+  }
+
+  addMarker(){
+    if(this.markerDraggable == 'yes'){
+      var isDraggable = true;
+    }else{
+      var isDraggable = false;
+    }
+
+    var newMarker = {
+      name: this.markerName,
+      lat: parseFloat(this.markerLat),
+      lng: parseFloat(this.markerLng),
+      draggable: isDraggable
+    }
+
+    this.markers.push(newMarker);
+
+    this.markerService.addMarker(newMarker);
+
+  }
+
+
+  removeMarker(marker){
+    for(let i = 0; i < this.markers.length; i++){
+      if(marker.lat == this.markers[i].lat && marker.lng == this.markers[i].lng){
+        this.markers.splice(i, 1)
+      }
+    }
+
+    this.markerService.removeMarker(marker)
+  }
+
+
 }
+
+//Marker Type
+interface marker {
+  name?: string;
+  lat: number
+  lng: number;
+  draggable: boolean;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AgmCoreModule} from "angular2-google-maps/core";
+import {MarkerService} from "./services/marker.service";
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBZTQEJmb21ZuDYnsYtLb4esEUs0fb6C6k'
+    })
   ],
-  providers: [],
+  providers: [MarkerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
